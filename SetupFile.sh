a9ee0859527f8a499e394cffc225277921f0f7d7
ram1123 <ramkrishna.sharma71@gmail.com>
2018-07-08 08:49:33 -0500

    Update scripts

---
+++ SetupFile.sh
@@ -0,0 +1,4 @@
+mv LimitSettingTool/fit.C $CMSSW_BASE/src/CombinedEWKAnalysis/CommonTools/data/anomalousCoupling/
+mv LimitSettingTool/config_TF1_wv_plain $CMSSW_BASE/src/CombinedEWKAnalysis/CommonTools/test/
+mv LimitSettingTool/resubmit.sh $CMSSW_BASE/src/CombinedEWKAnalysis/CommonTools/test/
+mv LimitSettingTool/runLimitTool.py $CMSSW_BASE/src/CombinedEWKAnalysis/CommonTools/test/
--- runLimitTool.py
+++ runLimitTool.py
@@ -1,21 +1,54 @@
-aQGCpars = {'fs0':[-5,5], 'fs1':[-5,5], 'ft0':[-5,5], 'ft1':[-5,5], 'ft2':[-5,5], 'fm0':[-5,5], 'fm1':[-5,5], 'fm6':[-5,5], 'fm7':[-5,5]}
+import os
 
+#aQGCpars = {'fs0':[-5,5], 'fs1':[-6,6], 'ft0':[-0.3,0.3], 'ft1':[-0.3,0.3], 'ft2':[-0.5,0.5], 'fm0':[-1.5,1.5], 'fm1':[-4,4], 'fm6':[-3,3], 'fm7':[-5.5,5.5]}
+aQGCpars = {'fs0':[-4.0,3.9]}
+#aQGCpars = {'fs0':[-4.5,4.5], 'ft0':[-0.2,0.2], 'ft1':[-0.3,0.3], 'ft2':[-0.5,0.5], 'fm0':[-1.5,1.5], 'fm1':[-4,4], 'fm6':[-3,3], 'fm7':[-5.5,5.5]}
+#for mT
+#aQGCpars = {'fs0':[-5,5], 'ft0':[-0.2,0.2], 'ft1':[-0.3,0.3], 'ft2':[-0.5,0.5], 'fm0':[-1.5,1.5], 'fm1':[-4,4], 'fm6':[-3,3], 'fm7':[-5.5,5.5]}
+#aQGCpars = {'fs0':[-5,5], 'ft0':[-3.7,3.7], 'ft1':[-3.7,3.7], 'ft2':[-3.7,3.7], 'fm0':[-3.5,3.5], 'fm1':[-4,4], 'fm6':[-3,3], 'fm7':[-3.5,3.5]}
+
+dataPath = "$CMSSW_BASE/src/CombinedEWKAnalysis/CommonTools/data/anomalousCoupling/"
+configFile = "config_TF1_wv_plain_my"
+os.system("date")
+
+LimitFile = "Limits_mass.log"
+
+os.system("rm Limits.log")
 
 for pars, value in aQGCpars.iteritems():
    print "\n\n","="*20,"\n\n"
-   print key,"\t",value[0],"\t",value[1]
+   print pars,"\t",value[0],"\t",value[1]
+   os.system('echo "" >> '+LimitFile)
+   os.system('echo "=====	'+pars+'      =====" >> '+LimitFile)
+   os.system('echo "" >> '+LimitFile)
 
-   command = 'cp ch1_splitted_TF1.root ch1_splitted_TF1_'+pars+'.root'
-   os.sys(command)
-   command = 'cp config_TF1_wv_plain config_TF1_wv_plain_'+pars
-   os.sys(command)
-   command = "sed 's/ft0/"+pars+"/g config_TF1_wv_plain_"+pars
-   os.sys(command)
-   
-   os.sys("python buildWorkspace_AC.py --config=config_TF1_wv_plain_"+pars)
 
-   os.sys("text2workspace.py -m 126 aC_ch1_splitted_TF1.txt -o Example_test.root -P CombinedEWKAnalysis.CommonTools.ACModel:par1_TF1_Model --PO channels=ch1_splitted_TF1 --PO poi=h"+pars+" --PO range_h"+pars+"=-5,5")
+   # copy root file
+   print "="*20,"\n\n\t COMMAND : 1 : COPY ROOT FILE \n\n","="*20
+   print('cp '+dataPath+'signal_proc_ch1_splitted_TF1_h'+pars+'.root '+dataPath+'signal_proc_ch1_splitted_TF1.root ')
+   print ""
+   os.system('cp '+dataPath+'signal_proc_ch1_splitted_TF1_h'+pars+'.root '+dataPath+'signal_proc_ch1_splitted_TF1.root')
 
-   os.sys("combine Example_test.root -M MultiDimFit -P "+ pars +" --floatOtherPOIs=0 --algo=grid --points=10000 --minimizerStrategy=2 -t -1 --expectSignal=1")
 
-   os.sys("python build1DInterval.py -5.0 5.0 higgsCombineTest.MultiDimFit.mH120.root h"+pars+"  > Log_"+pars+".log")
+   print "="*20,"\n\n\t COMMAND : 2 : COPY ROOT FILE \n\n","="*20
+   os.system("python buildWorkspace_AC.py --config="+configFile+"  | tee Limit_"+pars+"_mass.log")
+   print ""
+
+   print "="*20,"\n\n\t COMMAND : 3 : COPY ROOT FILE \n\n","="*20
+   os.system("text2workspace.py -m 126 aC_ch1_splitted_TF1.txt -o Example_test.root -P CombinedEWKAnalysis.CommonTools.ACModel:par1_TF1_Model --PO channels=ch1_splitted_TF1 --PO poi=hfs0 --PO range_hfs0="+str(value[0])+","+str(value[1]))
+   print ""
+
+   print "="*20,"\n\n\t COMMAND : 4 : COPY ROOT FILE \n\n","="*20
+   os.system("combine Example_test.root -M MultiDimFit -P hfs0 --floatOtherPOIs=0 --algo=grid --points=10000 --minimizerStrategy=2 -t -1 --expectSignal=1")
+   print ""
+
+   print "="*20,"\n\n\t COMMAND : 5 : Get Limits ",pars," \n\n","="*20
+   os.system("date")
+   print ""
+   print("python build1DInterval.py "+ str(value[0]) + "  " + str(value[1])  +" higgsCombineTest.MultiDimFit.mH120.root hfs0  >> "+LimitFile)
+   os.system("python build1DInterval.py "+ str(value[0]) + "  " + str(value[1])  +" higgsCombineTest.MultiDimFit.mH120.root hfs0  >> "+LimitFile)
+   print ""
+   os.system("date")
+
+   os.system("cp aC_ch1_splitted_TF1.txt aC_ch1_splitted_TF1_"+pars+"_mass.txt")
+os.system("date")
